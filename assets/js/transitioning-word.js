9943520c27ecb7efe2af596ca41e5cef5b41145f
harrisjm <hjmotiwala@utexas.edu>
2018-06-03 18:43:32 -0500

    fix transition word feature and add avater image

--- assets/js/transitioning-word.js
+++ assets/js/transitioning-word.js
@@ -1,15 +1,31 @@
 $(function() {
     var span = $('#transitioning-word');
-    var words = ['Web Developer', 'Math Enthusiast', 'Mozlem', 'Mufti-in-training',
-                 'Salafi hater', 'Belieber', 'Father of 3', 'Daddy'];
+    var words = ['Humza Baig','a Web Developer', 'a Math Enthusiast', 'a Student of Knowledge', 'a Machine Learning Engineer-in-training',
+                 'a Basketball Fan', 'a Gym Freak', 'a Videogame Expert', 'a Coffee Addict', 'a Hip-Hop Head', 'an Explorer', 'a Binge Watcher', 'an Anime Aficionado', 'a Software Engineer', 'a Full-Stack Developer', 'a Great Asset'];
+    var lastIdx = 0;
+    var arrLength = words.length;
 
     function changingTheWord() {
       span.fadeIn(1000);
       setTimeout(1000);
       span.fadeOut(1000);
 
-      span.text(words[Math.floor(Math.random() * 7)]);
+      var randNum = Math.floor(Math.random() * arrLength);
+
+      if (lastIdx == randNum) { //if same word
+        randNum++;
+
+        if (randNum >= arrLength) { //if out of bounds
+          randNum = 0;
+        }
+      }
+      lastIdx = randNum;
+      span.text(words[randNum]);
     }
 
-    setInterval(changingTheWord, 2000);
+    span.text(words[0]); //intial word before transitioning
+    setTimeout(1000);
+    span.fadeOut(1000);
+
+    setInterval(changingTheWord, 2750);
 });
